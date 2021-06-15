f84948afe467d52db1da3cb7b29a7470be14561a
Oscar Salamanca <osalamanca9558@gmail.com>
2021-06-15 17:57:07 -0500

    Greetings props
    correction

--- src/Components/ItemListContainer/ItemListContainer.js
+++ src/Components/ItemListContainer/ItemListContainer.js
@@ -1,11 +1,9 @@
 import React from 'react'
 import './ItemListContainer.css'
 
-export const ItemListContainer = ({ Name,LastName }) => {
-  Name= 'Oscar';
-  LastName= 'Salamanca';  
+export const ItemListContainer = ({ greetings }) => {
   return (
-    <div className="greetings"> Welcome!, {Name} {LastName}</div>
+    <div className="greetings"> Welcome!, {greetings} </div>
     )
 }
 export default ItemListContainer;
