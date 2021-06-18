8de0a5cd99903e6d56e179f2a2a98f290d1e3533
Oscar Salamanca <osalamanca9558@gmail.com>
2021-06-17 21:52:44 -0500

    Item and ItemList

--- src/Components/ItemListContainer/ItemListContainer.js
+++ src/Components/ItemListContainer/ItemListContainer.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import ItemList from '../Item/ItemList'
 import './ItemListContainer.css'
 
 export const ItemListContainer = ({ greetings }) => {
   return (
-    <div className="greetings"> Welcome, {greetings}!</div>
+    <div className="greetings"> Welcome, {greetings}!
+    <ItemList />
+    </div>
     )
 }
 
---
+++ src/components/Item/itemList.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Item from './item'
+
+
+function ItemList() {
+
+    return (
+        <div>
+<Item items={items}/>
+        </div>
+    )
+}
+
+export default ItemList
