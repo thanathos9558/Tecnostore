1f77a359f6b2ccb4677f1d30c8504f0840c31870
Oscar Alberto Salamanca Chacón <77991664+thanathos9558@users.noreply.github.com>
2021-06-18 08:41:00 -0500

    Update ItemCount.js
    bug fixed

--- src/Components/ItemCount/ItemCount.js
+++ src/Components/ItemCount/ItemCount.js
@@ -1,58 +1,58 @@
-import React, { useState } from 'react';
-import './ItemCount.css'
+import React, { useState } from "react";
+import "./ItemCount.css";
 
-export default class ItemCount extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      initial: 1,
-      stock: 5,
+function ItemCount(props) {
+  const [state, setState] = useState({
+    initial: 0,
+    stock: 5,
+    onAdd: 0
+  });
+
+  const handleAddItem = (e) => {
+    e.preventDefault();
+    const { initial, stock, onAdd } = state;
+    const newInitial = initial + 1;
+    if (stock >= newInitial) {
+      setState((prevState) => ({
+        ...prevState,
+        initial: newInitial,
+        onAdd: onAdd + 1
+      }));
+    }
+  };
+
+  const handleRemoveItem = (e) => {
+    e.preventDefault();
+    const { initial, onAdd } = state;
+    const newInitial = initial - 1;
+    if (newInitial >= 0) {
+      setState((prevState) => ({
+        ...prevState,
+        initial: newInitial,
+        onAdd: onAdd - 1
+      }));
+    }
+  };
+
+  const handleAddCar = (e) => {
+    e.preventDefault();
+    const { stock, onAdd } = state;
+    console.log(`Agregado ${onAdd} al carrito`);
+    setState(() => ({
+      stock: stock - onAdd,
+      initial: 0,
       onAdd: 0
-    };
-  }
-  render() {
-    return (
-      <div className="col mb-3">
-        <p className="cantidad">  {this.state.initial} </p>
-        <button onClick={() => this.setState({ initial: this.state.initial + 1 })}>
-          +
-        </button>
-        <button onClick={() => this.setState({ initial: this.state.initial - 1 })}>
-          -
-        </button>
-        <button onClick={(("Has agregado " + this.state.onAdd + " al carrito"))}>
-          Agregar al Carrito
-        </button>
-      </div>
-    );
-  }
+    }));
+  };
+
+  return (
+    <div className="plot">
+      <p className="cantidad"> {state.initial} </p>
+      <button onClick={handleAddItem}>+</button>
+      <button onClick={handleRemoveItem}>-</button>
+      <button onClick={handleAddCar}>Agregar al Carrito</button>
+    </div>
+  );
 }
-// Otra forma de Hacerlo
 
-// import React, { useState } from 'react';
-// import PropTypes from 'prop-types';
-// import './ItemCount.css'
-
-// const ItemCount = ({ value = 0 }) => {
-//   const [counter, setCounter] = useState(value);
-//   const handleAdd = () => {
-//     setCounter(counter + 1);
-//   }
-//   const handleSubtract = () => setCounter(counter - 1);
-//   const handleReset = () => setCounter(value);
-//   return (
-//     <div className="plot">
-//       <h1>ItemCount</h1>
-//       <h2> {counter} </h2>
-//       <button onClick={handleAdd}>+1</button>
-//       <button onClick={handleSubtract}>-1</button>
-//       <button onClick={handleReset}>Limpiar</button>
-//     </div>
-//   )
-// }
-
-// ItemCount.propTypes = {
-//   value: PropTypes.number
-// }
-
-// export default ItemCount;
+export default ItemCount;
